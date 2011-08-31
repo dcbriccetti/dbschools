d3e99ab72edb812bdf5a479b8d1cfdc9c667ff2d
Dave Briccetti <daveb@davebsoft.com>
2011-08-31 15:11:56 -0700

    Add cautionary code comment about not reordering

--- music/src/main/java/com/dbschools/music/events/TypeCode.java
+++ music/src/main/java/com/dbschools/music/events/TypeCode.java
@@ -2,4 +2,5 @@ package com.dbschools.music.events;
 
 public enum TypeCode {
     LOGIN, LOGOUT, SAVE_MUSICAN_MUSIC_GROUP, SAVE_OBJECT, UPDATE_OBJECT, DELETE_OBJECT, REMOVE_FROM_ALL_GROUPS
+    // Don't reorder or delete. Integer values are stored in database log table. Only add to the end.
 }
