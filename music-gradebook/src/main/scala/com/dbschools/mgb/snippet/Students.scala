6cff9d79c8923a5b1d80acb90fb023e4b90ff7a4
Dave Briccetti <daveb@davebsoft.com>
2012-10-16 20:54:46 -0700

    Move HTML out of snippet. (Thanks, Diego Medina, for the help.)

--- music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Students.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Students.scala
@@ -1,49 +1,28 @@
 package com.dbschools.mgb
 package snippet
 
-import scala.xml.{NodeSeq, Text}
 import org.squeryl.PrimitiveTypeMode._
 import net.liftweb._
 import util._
 import Helpers._
-import schema.AppSchema
+import schema.{Musician, Group, MusicianGroup, AppSchema}
 
 class Students {
 
   def render = {
-    <table>
-      <thead>
-        <tr>
-          <th>Graduation Year</th>
-          <th>Name</th>
-          <th>School Year</th>
-          <th>Group</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-        import AppSchema._
-        from (musicians, groups, musicianGroups)((m, g, mg) =>
-          where(m.musician_id === mg.musician_id and mg.group_id === g.group_id)
-          select((m, g, mg))
-          orderBy(mg.school_year, m.last_name, m.first_name, g.name)
-        ).map(r =>
-        <tr>
-          <td>
-            {r._1.graduation_year}
-          </td>
-          <td>
-            {r._1.last_name + ", " + r._1.first_name}
-          </td>
-          <td>
-            {r._3.school_year}
-          </td>
-          <td>
-            {r._2.name}
-          </td>
-        </tr>
-      )}
-      </tbody>
-    </table>
+    case class RowData(musician: Musician, group: Group, musicianGroup: MusicianGroup)
+    import AppSchema._
+    val rows = from (musicians, groups, musicianGroups)((m, g, mg) =>
+      where(m.musician_id === mg.musician_id and mg.group_id === g.group_id)
+      select(RowData(m, g, mg))
+      orderBy(mg.school_year, m.last_name, m.first_name, g.name)
+    )
+
+    "#studentRow" #> rows.map(row =>
+      ".gradYear *" #> row.musician.graduation_year &
+      ".stuName  *" #> (row.musician.last_name + ", " + row.musician.first_name) &
+      ".schYear  *" #> row.musicianGroup.school_year &
+      ".group    *" #> row.group.name
+    )
   }
 }
