16f36aeba687cb566e4179e801e6a369c7a47e4f
Dave Briccetti <daveb@davebsoft.com>
2012-10-21 21:20:48 -0700

    Remove unused database code

--- music-gradebook/src/main/scala/bootstrap/liftweb/Boot.scala
+++ music-gradebook/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -22,18 +22,6 @@ import com.dbschools.mgb.Db
  */
 class Boot {
   def boot {
-    if (!DB.jndiJdbcConnAvailable_?) {
-      val vendor = 
-	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
-			     Props.get("db.url") openOr 
-			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
-			     Props.get("db.user"), Props.get("db.password"))
-
-      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
-
-      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
-    }
-
     // where to search snippet
     LiftRules.addToPackages("com.dbschools.mgb")
 
