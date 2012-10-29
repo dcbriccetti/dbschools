12fd757439cc4832b90bdceabd937e1a6642b6dd
Dave Briccetti <daveb@davebsoft.com>
2012-10-28 22:25:16 -0700

    Partly implement move student to group/instrument

--- music-gradebook/src/main/scala/com/dbschools/mgb/snippet/StudentDetails.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/snippet/StudentDetails.scala
@@ -1,17 +1,19 @@
 package com.dbschools.mgb
 package snippet
 
+import xml.Text
 import scalaz._
 import Scalaz._
 import org.squeryl.PrimitiveTypeMode._
 import net.liftweb._
+import common.{Loggable, Empty}
 import util._
 import http._
-import schema._
-import scala.Some
-import schema.Musician
+import js._
+import net.liftweb.http.js.JsCmds._
+import schema.{Musician,GroupAssignment,GroupAssignments,Assessment,AppSchema}
 
-class StudentDetails {
+class StudentDetails extends Loggable {
   def render = {
     case class MusicianDetails(musician: Musician, groups: Iterable[GroupAssignment], assessments: Iterable[Assessment])
 
@@ -31,7 +33,8 @@ class StudentDetails {
       AppSchema.assessments.where(_.musician_id === musician.musician_id).toSeq))
 
     def makeGroups(ga: GroupAssignment) =
-      "* *" #> "%d: %s, %s".format(ga.musicianGroup.school_year, ga.group.name, ga.instrument.name)
+      "* *" #> (SHtml.ajaxCheckbox(false, checked => {Noop}) ++ Text(
+        "%d: %s, %s".format(ga.musicianGroup.school_year, ga.group.name, ga.instrument.name)))
 
     def makeDetails(md: MusicianDetails) =
       ".heading *"      #> "%s, %d, %d, %d".format(md.musician.name, md.musician.student_id,
@@ -43,5 +46,15 @@ class StudentDetails {
       }
 
     "#student" #> musicianDetailsItems.map(makeDetails)
+  }
+
+  def moveControls = {
+    def process(): JsCmd = {
+      logger.info("Move form submitted")
+      Noop
+    }
+    "#groups"       #> SHtml.ajaxSelect(AppSchema.groups.map(g => (g.group_id.toString, g.name)).toSeq, Empty, gid => {Noop}) &
+    "#instruments"  #> (SHtml.ajaxSelect(AppSchema.instruments.map(i => (i.instrument_id.toString, i.name)).toSeq, Empty, iid => {Noop})
+                         ++ SHtml.hidden(process))
   }
 }
