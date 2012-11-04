47313873ab22b54a71da99336ad9ad3605a88f9b
Dave Briccetti <daveb@davebsoft.com>
2012-11-04 10:56:01 -0800

    Only encrypt those passwords that need it

--- music-gradebook/src/main/scala/com/dbschools/mgb/convert/EncryptPasswords.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/convert/EncryptPasswords.scala
@@ -2,16 +2,23 @@ package com.dbschools.mgb.convert
 
 import net.liftweb.util.BCrypt
 import org.squeryl.PrimitiveTypeMode._
+import net.liftweb.common.Loggable
 import com.dbschools.mgb.Db
 import com.dbschools.mgb.schema.AppSchema
 
 /** Do a one-time encryption of the plaintext passwords in the users table */
-object EncryptPasswords {
+object EncryptPasswords extends Loggable {
   def apply() {
     Db.initialize()
     transaction {
-      val updatedUsers = AppSchema.users.map(u => u.copy(epassword = BCrypt.hashpw(u.password, BCrypt.gensalt())))
-      AppSchema.users update updatedUsers
+      val query = AppSchema.users.where(_.epassword === "")
+      val updatedUsers = query.map(u => u.copy(epassword = BCrypt.hashpw(u.password, BCrypt.gensalt())))
+      if (updatedUsers.isEmpty)
+        logger.info("All passwords are already encrypted")
+      else {
+        logger.info("Encrypting passwords for " + updatedUsers.map(_.login).toSeq.sorted.mkString(", "))
+        AppSchema.users update updatedUsers
+      }
     }
   }
 }
