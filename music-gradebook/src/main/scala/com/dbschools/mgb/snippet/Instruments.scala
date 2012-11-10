21e5da779991680665c8f7ce77f9324604c7a4e2
Carlos Montemuiño <carlos.montemuino@gmail.com>
2012-11-10 18:58:22 +0100

    Implemented Instrument page with CRUD operations.
    Signed-off-by: Carlos Montemuiño <carlos.montemuino@gmail.com>

---
+++ music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Instruments.scala
@@ -0,0 +1,102 @@
+package com.dbschools.mgb.snippet
+
+import org.squeryl.PrimitiveTypeMode._
+import net.liftweb._
+import net.liftweb.common.Loggable
+import util._
+import com.dbschools.mgb.schema.{AppSchema, Instrument}
+import net.liftweb.http.RequestVar
+import net.liftweb.http.SHtml
+import net.liftweb.http.S
+import scala.xml.Text
+import net.sf.cglib.proxy.NoOp
+import net.liftweb.record.field.IntField
+
+/** Snippet for Instrument CRUD operations. */
+class Instruments extends Loggable {
+  
+  /** Used to pass Instrument between successive HTTP Requests.*/
+  object requestData extends RequestVar[Instrument](Instrument.createRecord)
+  
+  def list = {
+    ".row *"   #> AppSchema.instruments.map(instrument => {
+      ".instrumentSequence *"   #> instrument.sequence.is &
+      ".instrumentName *"       #> instrument.name.is &
+      ".actions *"    #> {
+        SHtml.link("/instruments/view", () => requestData(instrument), Text("View")) ++ Text(" ") ++
+        SHtml.link("/instruments/edit", () => requestData(instrument), Text("Edit")) ++ Text(" ") ++
+        SHtml.link("/instruments/delete", () => requestData(instrument), Text("Delete"))
+      }
+    })
+  }
+  
+  def create = {
+    val instrument = requestData.is
+    "#hidden" #> SHtml.hidden(() => requestData(instrument)) &
+    "#instrumentSequence" #> SHtml.number(requestData.is.sequence.is, (sequence: Int) => requestData.is.sequence(sequence), 0, 200) &
+    "#instrumentName" #> SHtml.text(requestData.is.name.is, name => requestData.is.name(name)) &
+    "#submit" #> SHtml.onSubmitUnit(processCreate)
+  }
+  
+  def delete = {
+    if (!requestData.set_?) {
+      logger.info("Delete Instrument page has not been reached from Instrument List page. Redirecting to List page.")
+      S.redirectTo("/instruments/list")
+    }
+    
+    val instrument = requestData.is
+
+    "#instrumentName" #> instrument.name.is &
+      "#yes" #> SHtml.link("/instruments/list", () => {
+        AppSchema.instruments.deleteWhere(ins => ins.idField.is === instrument.idField.is)
+      }, Text("Yes")) &
+      "#no" #> SHtml.link("/instruments/list", () => {}, Text("No"))
+  }
+  
+  def edit = {
+    if (!requestData.set_?) {
+      logger.info("Edit Instrument page has not been reached from Instrument List or View Instrument page. Redirecting to List page.")
+      S.redirectTo("/instruments/list")
+    }
+    
+    val instrument = requestData.is
+    "#hidden" #> SHtml.hidden(() => requestData(instrument)) &
+    "#instrumentSequence" #> SHtml.number(requestData.is.sequence.is, (sequence: Int) => requestData.is.sequence(sequence), 0, 200) &
+    "#instrumentName" #> SHtml.text(requestData.is.name.is, name => requestData.is.name(name)) &
+    "#submit" #> SHtml.onSubmitUnit(processEdit)
+  }
+  
+  def view = {
+    if (!requestData.set_?) {
+      logger.info("View Instrument page has not been reached from Instrument List. Redirecting to List page.")
+      S.redirectTo("/instruments/list")
+    }
+    
+    val instrument = requestData.is
+    "#instrumentSequence" #> requestData.is.sequence.asHtml &
+    "#instrumentName" #> requestData.is.name.asHtml &
+    "#edit" #> SHtml.link("/instruments/edit", () => requestData(instrument), Text("Edit"))
+  }
+  
+  def processCreate() = {
+    requestData.is.validate match {
+      case Nil => {
+        AppSchema.instruments.insert(requestData.is)
+        S.notice("Instrument has been saved")
+        S.seeOther("/instruments/list")
+      }
+      case errors => S.error(errors)
+    }
+  }
+  
+  def processEdit() = {
+    requestData.is.validate match {
+      case Nil => {
+        AppSchema.instruments.update(requestData.is)
+        S.notice("Instrument has been saved")
+        S.seeOther("/instruments/list")
+      }
+      case errors => S.error(errors)
+    }
+  }
+}
