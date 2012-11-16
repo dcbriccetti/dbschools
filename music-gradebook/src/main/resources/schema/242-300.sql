d81608ec713c6365e9de39c7733a39aa0520e315
Dave Briccetti <daveb@davebsoft.com>
2012-11-16 09:21:33 -0800

    Add a sequence needed for inserting instruments

--- music-gradebook/src/main/resources/schema/242-300.sql
+++ music-gradebook/src/main/resources/schema/242-300.sql
@@ -1,2 +1,4 @@
 alter table music_user add column epassword varchar not null default '';
 alter table music_user add column enabled boolean not null default 't';
+
+create sequence s_instrument_instrument_id;
