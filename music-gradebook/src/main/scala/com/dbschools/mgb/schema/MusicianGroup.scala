56314141f00d2321d4d2d16caa2accaf653fc5ee
Dave Briccetti <daveb@davebsoft.com>
2012-11-22 14:49:46 -0800

    Add a method to return all musicians in a group in the current term

--- music-gradebook/src/main/scala/com/dbschools/mgb/schema/MusicianGroup.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/schema/MusicianGroup.scala
@@ -1,5 +1,8 @@
 package com.dbschools.mgb.schema
 
+import org.squeryl.PrimitiveTypeMode._
+import com.dbschools.mgb.model.Terms
+
 case class MusicianGroup(
   id:             Int,
   musician_id:    Int,
@@ -7,3 +10,10 @@ case class MusicianGroup(
   instrument_id:  Int,
   school_year:    Int
 )
+
+object MusicianGroup {
+  def musiciansInCurrentTerm =
+    from(AppSchema.musicianGroups, AppSchema.musicians)((mg, m) =>
+    where(mg.musician_id === m.musician_id.is and mg.school_year === Terms.currentTerm)
+    select(m))
+}
