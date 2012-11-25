7bf043e1feeff9a9b13aac1f68bddf221aeeaf0b
Dave Briccetti <daveb@davebsoft.com>
2012-11-24 18:25:34 -0800

    Reduce variable scope

--- music-gradebook/src/main/scala/com/dbschools/mgb/snippet/History.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/snippet/History.scala
@@ -10,9 +10,11 @@ class History {
 
   def assessmentsByYearAndGroup = {
     case class WithGroupName(tg: TermGroupAssessments, name: String)
-    val groups = AppSchema.groups.map(g => g.group_id -> g).toMap
-    val withNames = schema.Group.groupsWithAssessments.map(t => WithGroupName(t, groups(t.groupId).name))
-    val sortedWithNames = withNames.toSeq.sortBy(_.name).sortWith(_.tg.term > _.tg.term)
+    val sortedWithNames = {
+      val groups = AppSchema.groups.map(g => g.group_id -> g).toMap
+      val withNames = schema.Group.groupsWithAssessments.map(t => WithGroupName(t, groups(t.groupId).name))
+      withNames.toSeq.sortBy(_.name).sortWith(_.tg.term > _.tg.term)
+    }
     val nf = java.text.NumberFormat.getInstance
 
     "#row" #> sortedWithNames.map(wn =>
