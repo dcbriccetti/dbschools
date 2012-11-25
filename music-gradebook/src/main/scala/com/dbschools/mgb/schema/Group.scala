fb8d491247679f77e162007da04173838f611f37
Dave Briccetti <daveb@davebsoft.com>
2012-11-24 16:46:03 -0800

    Only offer the groups which have assessments in the selected term

--- music-gradebook/src/main/scala/com/dbschools/mgb/schema/Group.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/schema/Group.scala
@@ -1,10 +1,22 @@
 package com.dbschools.mgb.schema
 
 import org.squeryl.annotations.Column
+import org.squeryl.PrimitiveTypeMode._
 
 case class Group(
-  group_id:   Int,
-  name:       String,
-  @Column("does_testing")
-  doesTesting: Boolean
+                          group_id:     Int,
+                          name:         String,
+  @Column("does_testing") doesTesting:  Boolean
 )
+
+object Group {
+  /** Maps from terms to the IDs of groups that have assessments in that term */
+  lazy val groupsWithAssessmentsByTerm: Map[Int, Seq[Int]] =
+    from(AppSchema.assessments, AppSchema.musicianGroups)((a, mg) =>
+      where(a.musician_id === mg.musician_id)
+      groupBy(mg.school_year, mg.group_id)
+      compute(count(a.assessment_id))
+    ).map(g => TermGroup(g.key._1, g.key._2)).groupBy(_.term).mapValues(_.map(_.groupId).toSeq)
+}
+
+case class TermGroup(term: Int, groupId: Int)
--- music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Selectors.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Selectors.scala
@@ -1,39 +1,53 @@
-package com.dbschools.mgb.snippet
+package com.dbschools.mgb
+package snippet
 
 import scalaz._
 import Scalaz._
 import org.squeryl.PrimitiveTypeMode._
-import net.liftweb.common.Loggable
+import net.liftweb.common.{Empty, Loggable, Full}
 import net.liftweb.http.SHtml
-import net.liftweb.common.Full
+import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.JsCmds.ReplaceOptions
+import net.liftweb.http
+import http.js.JsCmds.Noop
 import com.dbschools.mgb.model.Terms
 import com.dbschools.mgb.schema.{MusicianGroup, AppSchema}
-import net.liftweb.http.js.JsCmd
 
 class Selectors(changed: () => JsCmd, onlyTestingGroups: Boolean = false) extends Loggable {
   var opSelectedTerm: Option[Int] = Some(Terms.currentTerm) // None means no specific term, therefore all
   var opSelectedGroupId = none[Int]                         // None means no specific group, therefore all
   var opSelectedInstId  = none[Int]                         // None means no specific instrument, therefore all
 
-  def yearSelector = selector(Terms.allTermsFormatted, opSelectedTerm, opSelectedTerm = _)
+  val yearSelector = selector("yearSelector", Terms.allTermsFormatted, opSelectedTerm, updateTerm)
 
-  def groupSelector = selector(groups.map(g => g.group_id.toString -> g.name),
-    opSelectedGroupId, opSelectedGroupId = _)
-
-  private def groups = {
-    val g = AppSchema.groups.toList
-    if (onlyTestingGroups) g.filter(_.doesTesting) else g
+  private def updateTerm(opTerm: Option[Int]) = {
+    opSelectedTerm = opTerm
+    // Show only the groups with assessments in the term
+    ReplaceOptions(groupSelectorId, groupSelectValues, Empty): JsCmd
   }
 
-  def instrumentSelector = selector(AppSchema.instruments.toList.map(i => i.id.toString -> i.name.is),
+  val groupSelectorId: String = "groupSelector"
+
+  val groupSelector =
+    selector(groupSelectorId, groupSelectValues, opSelectedGroupId, opSelectedGroupId = _)
+
+  private def groupSelectValues: List[(String, String)] = {
+    val groups = AppSchema.groups.toList
+    val opGroupIds = opSelectedTerm.map(schema.Group.groupsWithAssessmentsByTerm)
+    val filtered = opGroupIds.map(ids => groups.filter(g => ids.contains(g.group_id))) | groups
+    filtered.sortBy(_.name).map(g => g.group_id.toString -> g.name)
+  }
+
+  val instrumentSelector = selector("instrumentSelector",
+    AppSchema.instruments.toList.map(i => i.id.toString -> i.name.is),
     opSelectedInstId, opSelectedInstId = _)
 
-  private def selector(items: List[(String, String)], opId: Option[Int], fn: (Option[Int]) => Unit) = {
+  private def selector(id: String, items: List[(String, String)], opId: Option[Int], fn: (Option[Int]) => JsCmd) = {
     val All = "All"
     SHtml.ajaxSelect((All, All) :: items, Full(opId.map(_.toString) | All), sel => {
-      fn(if (sel == All) None else Some(sel.toInt))
+      fn(if (sel == All) None else Some(sel.toInt)) &
       changed()
-    })
+    }, "id" -> id)
   }
 
   def musicianGroups = MusicianGroup.selectedMusicians(opSelectedTerm, opSelectedGroupId, opSelectedInstId)
