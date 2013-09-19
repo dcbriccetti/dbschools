e2721bbd3bb5f81c03356ab31422848eaa6a199c
Dave Briccetti <daveb@davebsoft.com>
2013-09-18 19:00:28 -0700

    All running stats for any or all years. Format numbers.

--- music-gradebook/src/main/scala/com/dbschools/mgb/model/Terms.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/model/Terms.scala
@@ -17,15 +17,6 @@ object Terms {
     (if (n.getMonthOfYear >= yearEndMonth) n else n.minus(1.year)).getYear
   }
 
-  def termFromTo(term: Int = currentTerm) = {
-    val dtFrom = {
-      val n = DateTime.now.withDayOfMonth(1)
-      if (n.getMonthOfYear >= yearEndMonth) n.withMonth(yearEndMonth) else n.withMonth(yearEndMonth).minus(1.year)
-    }
-    val dtTo = dtFrom + 1.year
-    (dtFrom, dtTo)
-  }
-
   def graduationYearAsGrade(graduationYear: Int, schoolYear: Int = currentTerm) =
     8 - (graduationYear - schoolYear)
 
@@ -46,5 +37,14 @@ object Terms {
 
   def toTs(dt: DateTime) = new Timestamp(dt.millis)
 
-  def termEnd(term: Int) = new DateTime(term + 1, yearEndMonth, 1, 0, 0, 0, 0)
+  def termStart(term: Int) = new DateTime(term, yearEndMonth, 1, 0, 0, 0, 0)
+
+  def termEnd(term: Int) = termStart(term) + 1.year
+
+  def termFromTo(term: Int = currentTerm) = (termStart(term), termEnd(term))
+
+  def allTermsFromTo = {
+    val at = allTerms.toSeq
+    (termStart(at.min), termEnd(at.max))
+  }
 }
--- music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Statistics.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Statistics.scala
@@ -1,46 +1,75 @@
 package com.dbschools.mgb
 package snippet
 
+import scalaz._
+import Scalaz._
 import org.squeryl.{PrimitiveTypeMode, Query}
 import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.dsl.GroupWithMeasures
 import net.liftweb._
 import util.BindHelpers._
+import net.liftweb.http.Templates
+import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.js.JsCmds.Replace
 import schema.AppSchema
 import AppSchema.{groups, musicianGroups, musicians, assessments, users}
 import model.Terms
 import model.Terms.toTs
+import net.liftweb.common.Loggable
+import java.text.NumberFormat
 
-class Statistics {
+class Statistics extends Loggable {
 
-  private val (dtFrom, dtTo) = Terms.termFromTo()
+  private val selectors = new Selectors(() => replaceContents)
+
+  private def replaceContents = {
+    val template = "_statsData"
+    Templates(List(template)).map(Replace("statsData", _)) openOr {
+      logger.error("Error loading template " + template)
+      Noop
+    }
+  }
+
+  def yearSelector = selectors.yearSelector
+
+  private def fromTo = selectors.opSelectedTerm.map(Terms.termFromTo) | Terms.allTermsFromTo
 
   def assessmentsByGroup  = createTable("Group",  queryByGroup)
-  def assessmentsByGrade  = createTable("Grade",  queryByGrade,
-    (gradYear: String) => Terms.graduationYearAsGrade(gradYear.toInt).toString)
+  def assessmentsByGrade  = createTable("Grade",  queryByGrade, (gradYear: String) =>
+    (Terms.graduationYearAsGrade(gradYear.toInt) -
+    (Terms.currentTerm - (selectors.opSelectedTerm | Terms.currentTerm))).toString)
   def assessmentsByTester = createTable("Tester", queryByTester)
 
   private type QueryGm = Query[GroupWithMeasures[PrimitiveTypeMode.StringType, PrimitiveTypeMode.LongType]]
 
-  private def queryByGroup(pass: Boolean) = from(groups, musicianGroups, musicians, assessments)((g, mg, m, a) =>
-    where(g.id === mg.group_id and mg.musician_id === m.musician_id.is and mg.school_year === dtFrom.getYear
-      and m.musician_id.is === a.musician_id
-      and a.pass === pass and a.assessment_time.between(toTs(dtFrom), toTs(dtTo)))
-    groupBy(g.name)
-    compute(count(a.assessment_id))
-  )
+  private def queryByGroup(pass: Boolean) = {
+    val (dtFrom, dtTo) = fromTo
+    from(groups, musicianGroups, musicians, assessments)((g, mg, m, a) =>
+      where(g.id === mg.group_id and mg.musician_id === m.musician_id.is and mg.school_year === dtFrom.getYear
+        and m.musician_id.is === a.musician_id
+        and a.pass === pass and a.assessment_time.between(toTs(dtFrom), toTs(dtTo)))
+        groupBy(g.name)
+        compute(count(a.assessment_id))
+    )
+  }
 
-  private def queryByGrade(pass: Boolean) = from(musicians, assessments)((m, a) =>
-    where(m.musician_id.is === a.musician_id and a.pass === pass and a.assessment_time.between(toTs(dtFrom), toTs(dtTo)))
-    groupBy(m.graduation_year.is.toString)
-    compute(count(a.assessment_id))
-  )
+  private def queryByGrade(pass: Boolean) = {
+    val (dtFrom, dtTo) = fromTo
+    from(musicians, assessments)((m, a) =>
+      where(m.musician_id.is === a.musician_id and a.pass === pass and a.assessment_time.between(toTs(dtFrom), toTs(dtTo)))
+        groupBy(m.graduation_year.is.toString)
+        compute(count(a.assessment_id))
+    )
+  }
 
-  private def queryByTester(pass: Boolean) = from(users, assessments)((u, a) =>
-    where(u.id === a.user_id and a.pass === pass and a.assessment_time.between(toTs(dtFrom), toTs(dtTo)))
-    groupBy(u.last_name)
-    compute(count(a.assessment_id))
-  )
+  private def queryByTester(pass: Boolean) = {
+    val (dtFrom, dtTo) = fromTo
+    from(users, assessments)((u, a) =>
+      where(u.id === a.user_id and a.pass === pass and a.assessment_time.between(toTs(dtFrom), toTs(dtTo)))
+        groupBy(u.last_name)
+        compute(count(a.assessment_id))
+    )
+  }
 
   private def identityKeyTransformer(key: String) = key
 
@@ -55,6 +84,9 @@ class Statistics {
     css(rowHeading, passesMap.keys.toSet ++ failuresMap.keys.toSet, passesMap, failuresMap)
   }
 
+  private val numf = NumberFormat.getNumberInstance
+  private def f[A](num: A) = numf.format(num)
+
   private def css(rowHeading: String, groupNames: Iterable[String],
       passesMap: Map[String, Long], failuresMap: Map[String, Long]) =
     ".rowHeading *"   #> rowHeading &
@@ -63,9 +95,9 @@ class Statistics {
       val failures = failuresMap.getOrElse(x, 0L)
       val total = passes + failures
       ".rowName    *" #> x &
-      ".asses      *" #> total &
-      ".pass       *" #> passes &
-      ".fail       *" #> failures &
+      ".asses      *" #> f(total) &
+      ".pass       *" #> f(passes) &
+      ".fail       *" #> f(failures) &
       ".pctPass    *" #> "%.2f".format(passes * 100.0 / total)
     })
 }
