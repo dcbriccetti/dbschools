80fe8d4fe6998f84caa61af685453ce4cd460531
Dave Briccetti <daveb@davebsoft.com>
2013-10-31 13:52:56 -0700

    Improve appearance, including using Bootstrap table and grid stylings

--- music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Assessments.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Assessments.scala
@@ -7,22 +7,28 @@ import org.scala_tools.time.Imports._
 import net.liftweb._
 import util._
 import Helpers._
-import model.{AssessmentRow, AssessmentRows}
+import model.AssessmentRows
 import schema.Subinstrument
 
 class Assessments extends MusicianFromReq {
-  def render = ".assessmentRow" #> {
-    val dtf = DateTimeFormat.forStyle("S-")
-    val tmf = DateTimeFormat.forStyle("-M")
+  def render =
+    opMusician.map(m => AssessmentRows.forMusician(m.id)).toList.flatten match {
+      case Nil  => ClearNodes
+      case rows =>
+        ".assessmentRow" #> {
+          val dtf = DateTimeFormat.forStyle("S-")
+          val tmf = DateTimeFormat.forStyle("-M")
 
-    val rows = opMusician.map(_.id).map(AssessmentRows.forMusician) | Seq[AssessmentRow]()
-    rows.map(ar =>
-      ".date       *"   #> <span title={tmf.print(ar.date)}>{dtf.print(ar.date)}</span> &
-      ".tester     *"   #> ar.tester &
-      ".piece [class]"  #> (if (ar.pass) "pass" else "fail") &
-      ".piece      *"   #> ar.piece &
-      ".instrument *"   #> (ar.instrument + ~ar.subinstrument.map(Subinstrument.suffix)) &
-      ".comments   *"   #> ar.notes
-    )
-  }
+          rows.map(ar =>
+            ".date       *" #> <span title={tmf.print(ar.date)}>
+              {dtf.print(ar.date)}
+            </span> &
+              ".tester     *" #> ar.tester &
+              ".piece [class]" #> (if (ar.pass) "pass" else "fail") &
+              ".piece      *" #> ar.piece &
+              ".instrument *" #> (ar.instrument + ~ar.subinstrument.map(Subinstrument.suffix)) &
+              ".comments   *" #> ar.notes
+          )
+        }
+    }
 }
