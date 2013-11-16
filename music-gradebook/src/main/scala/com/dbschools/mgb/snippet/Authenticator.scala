f9e2560168ce108b0babdef8f4e98639af636ebf
Dave Briccetti <daveb@davebsoft.com>
2013-11-15 23:23:41 -0800

    Don’t require password for demo system.

--- music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Authenticator.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/snippet/Authenticator.scala
@@ -10,13 +10,13 @@ import net.liftweb.http.{S, SessionVar, SHtml}
 import net.liftweb.util.Helpers._
 import net.liftweb.util.{PassThru, ClearNodes, Props, BCrypt}
 import net.liftweb.http.js.JsCmds.{Script, Noop, RedirectTo, FocusOnLoad}
-import com.dbschools.mgb.schema.AppSchema
-import com.dbschools.mgb.model.RunState
+import schema.AppSchema
+import model.RunState
 
 class Authenticator extends FormHelper {
   val log = Logger.getLogger(getClass)
   import bootstrap.liftweb.ApplicationPaths._
-  import Authenticator.{credentialsValid, userName}
+  import Authenticator.{credentialsValid, isDemo, userName}
   
   var password = ""
 
@@ -29,7 +29,7 @@ class Authenticator extends FormHelper {
     "#userFormGroup"      #> addFormGroup("userName", "User Name", userText) &
     "#passwordFormGroup"  #> addFormGroup("password", "Password", passwordText) &
     "#submit"             #> SHtml.submit("Log In", () => {
-      if (credentialsValid(password)) {
+      if (isDemo || credentialsValid(password)) {
         RunState loggedIn true
         log.info(s"${userName.is} logged in")
         S.redirectTo(groups.href)
@@ -37,12 +37,12 @@ class Authenticator extends FormHelper {
         log.info(s"${userName.is} failed to log in")
         S.error("Login failed")
       }
-    }, "id" -> "submit", "class" -> "btn btn-default")
+    })
 
 
-  def demoMsg = if (Authenticator.isDemo) PassThru else ClearNodes
+  def demoMsg = if (isDemo) PassThru else ClearNodes
 
-  def organization = if (Authenticator.isDemo) ClearNodes else "#organization" #> Text(Authenticator.org)
+  def organization = if (isDemo) ClearNodes else "#organization" #> Text(Authenticator.org)
 
   def logOut = {
     Authenticator.logOut()
@@ -56,7 +56,7 @@ class Authenticator extends FormHelper {
 object Authenticator {
   val org = ~Props.get("organization").toOption
   val isDemo = org == "demo"
-  private val startingUserName = if (Authenticator.isDemo) "a" else ""
+  private val startingUserName = if (isDemo) "a" else ""
 
   object userName extends SessionVar(startingUserName)
 
