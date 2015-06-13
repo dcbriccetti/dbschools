b3d046a19053b281dd9e8f1d6223be97a405842d
Dave Briccetti <daveb@davebsoft.com>
2015-06-13 01:25:17 -0700

    Update class periods for next year

--- music-gradebook/src/main/scala/com/dbschools/mgb/model/Periods.scala
+++ music-gradebook/src/main/scala/com/dbschools/mgb/model/Periods.scala
@@ -1,6 +1,5 @@
 package com.dbschools.mgb.model
 
-import org.joda.time.DateTimeConstants
 import org.scala_tools.time.Imports._
 
 object Periods {
@@ -19,39 +18,62 @@ object Periods {
     def startMs = start.toDateTime.millis
     def endMs = end.toDateTime.millis
   }
+
+  /** “Block period” */
+  case class Bp(sh: Int, sm: Int, eh: Int, em: Int)
+  
   object Period {
     def apply(num: Int, sh: Int, sm: Int, eh: Int, em: Int): Period = Period(num, SimpleTime(sh, sm), SimpleTime(eh, em))
+    def apply(num: Int, bp: Bp): Period = 
+      Period(num, SimpleTime(bp.sh, bp.sm), SimpleTime(bp.eh, bp.em))
   }
 
-  // todo Create database tables and import feature
-  private val periodsNormal = Vector(
+  private val monFri = Vector(
     Period(1, 8, 15, 9, 2),
-    Period(2, 9, 5, 9, 50),
-    Period(3, 9, 53, 10, 38),
+    Period(2, 9, 6, 9, 51),
+    Period(3, 9, 55, 10, 40),
     Period(4, 10, 56, 11, 41),
-    Period(5, 11, 44, 12, 29),
-    Period(6, 13, 20, 14, 5),
-    Period(7, 14, 8, 14, 55),
-    Period(8, 14, 58, 15, 43)
+    Period(5, 11, 45, 12, 30),
+    Period(6, 13, 19, 14, 4),
+    Period(7, 14, 8, 14, 53)
   )
 
-  private val periodsWeds = Vector(
-    Period(1, 9, 0, 9, 40),
-    Period(2, 9, 43, 10, 23),
-    Period(3, 10, 38, 11, 18),
-    Period(4, 11, 21, 12, 1),
-    Period(5, 12, 4, 12, 44),
-    Period(6, 13, 30, 14, 10),
-    Period(7, 14, 13, 14, 53),
-    Period(8, 14, 56, 15, 41)
+  private val bp1 = Bp(8, 15, 9, 22)
+  private val bp2 = Bp(9, 26, 10, 31)
+  private val bp3 = Bp(10, 45, 11, 50)
+  private val bp4 = Bp(12, 39, 13, 44)
+  private val bp5 = Bp(13, 48, 14, 53)
+
+  private val tue = Vector(
+    Period(1, bp1),
+    Period(3, bp2),
+    Period(5, bp3),
+    Period(6, bp4),
+    Period(7, bp5)
   )
 
+  private val wed = Vector(
+    Period(2, 9, 24, 10, 31),
+    Period(4, bp3),
+    Period(5, bp4),
+    Period(6, bp5)
+  )
+
+  private val thu = Vector(
+    Period(1, bp1),
+    Period(2, bp2),
+    Period(3, bp3),
+    Period(4, bp4),
+    Period(7, bp5)
+  )
+  
+  private val week = Vector(monFri, tue, wed, thu, monFri)
 
   def periodWithin: TimeClass = if (testingState.specialSchedule) InSpecialSchedule else
     periodsToday.find(_.within(DateTime.now)) getOrElse NotInPeriod
 
   private def periodsToday = DateTime.now.getDayOfWeek match {
-    case DateTimeConstants.WEDNESDAY  => periodsWeds
-    case _                            => periodsNormal
+    case d if d >= 1 && d <= 5 => week(d - 1)
+    case _                     => week.head // Use Monday for out of range
   }
 }
