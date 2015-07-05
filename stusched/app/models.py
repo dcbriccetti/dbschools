239babd2e8da37732ab8b831348f227f0cb49d1a
Dave Briccetti <daveb@davebsoft.com>
2015-07-05 11:12:11 -0700

    Add a template

--- stusched/app/models.py
+++ stusched/app/models.py
@@ -7,13 +7,20 @@ class Course(models.Model):
     def __str__(self):
         return self.name.__str__()
 
+    def description_with_br(self):
+        return self.description.replace('\n', '<br/>')
+
 class Section(models.Model):
-    start_date = models.DateTimeField()
+    start_time = models.DateTimeField()
+    duration = models.DurationField()
+    num_days = models.DecimalField(max_digits=3, decimal_places=0, default=1)
     course = models.ForeignKey(Course)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    min_students = models.DecimalField(max_digits=3, decimal_places=0, default=3)
+    max_students = models.DecimalField(max_digits=3, decimal_places=0, default=6)
 
     def __str__(self):
-        return "%s on %s" % (self.course.name, self.start_date.__str__())
+        return "At %s" % (self.start_time.__str__())
 
 class Parent(models.Model):
     name = models.CharField(max_length=100)
--- stusched/app/views.py
+++ stusched/app/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Course, Section
+
+class ScheduledCourse(object):
+    def __init__(self, name, description, sections):
+        self.name = name
+        self.description = description
+        self.sections = sections
+
+    def __str__(self, *args, **kwargs):
+        return self.name + ' ' + self.description
+
 
 def index(request):
-    return HttpResponse('Hi. Please enjoy this.')
+    sections = Section.objects.order_by('start_time')
+    scheduledCourses = set((s.course for s in sections))
+    courses = (ScheduledCourse(c.name, c.description_with_br(),
+        [s for s in sections if s.course == c]) for c in Course.objects.order_by('name') if c in scheduledCourses)
+
+    return render(request, 'app/index.html', {'courses': courses})
