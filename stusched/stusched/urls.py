1c87fb83859af6e549cc4309c31b9a190f8396ff
Dave Briccetti <daveb@davebsoft.com>
2016-07-23 03:47:33 -0700

    Remove stusched, since it now has its own repository

--- stusched/stusched/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from django.views.generic import RedirectView
-
-urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/app/', permanent=False), name='index'),
-    url(r'^app/', include('app.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-]
