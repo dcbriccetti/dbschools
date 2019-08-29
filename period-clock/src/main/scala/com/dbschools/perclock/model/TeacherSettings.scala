aad30f17015664bfdac872085de46381ebdc5af7
Dave Briccetti <daveb@davebsoft.com>
2019-08-29 14:57:04 -0700

    Update Bob Athayde’s schedule

--- period-clock/src/main/scala/com/dbschools/perclock/model/TeacherSettings.scala
+++ period-clock/src/main/scala/com/dbschools/perclock/model/TeacherSettings.scala
@@ -7,14 +7,13 @@ object TeacherSettings {
 
   val DefaultSettings = (DefaultPeriodNames, DefaultFillColors, DefaultTextColors)
   val MusicSettings = (
-    "Bronze |Silver|Symph.|Cadet 4|Cadet 5|Chrs./Str.|Orch.",
-    "#CD7F32 silver gold   indigo  blue    yellow     green",
-    "black  black   black  white   white   black      white"
+    "Bronze |Silver|Cadet |Symph.|Chorus |Strings|Orch.",
+    "#CD7F32 silver indigo gold   yellow  blue    green",
+    "black   black  white  black  black   white   white"
     )
   val PredefinedSettings = Map(
     "bathayde" -> MusicSettings,
     "lmcnulty" -> MusicSettings,
     "cwindfuh" -> ("German 1B|German 1A|Wheel 6|Wheel 6|French 1A|French 1A|7", DefaultFillColors, DefaultTextColors)
   )
-
 }
